0403abfe7ba68a416372ff9e352d6510d029d996
aashi54 <asmitapatidar2023@gmail.com>
2023-12-20 13:05:23 +0530

    Rest API (post and get) implemented

--- node/short-url/controllers/url.js
+++ node/short-url/controllers/url.js
@@ -1,5 +1,6 @@
 
 const shortid = require("shortid")
+const URL = require("../models/url")
 
 async function handleGenerateNewShortURL(req,res){
   const body = req.body;
@@ -8,11 +9,18 @@ async function handleGenerateNewShortURL(req,res){
 
   await URL.create({
     shortId: shortID,
-    redirectURL : body.URL,
+    redirectURL : body.url,
+   
     visitedHistory: [],
   })
 
   return res.json({id: shortID})
 }
 
-module.exports = {handleGenerateNewShortURL}
+async function handleGetAnalytics(req,res){
+    const shortId = req.params.shortId
+    const result = await URL.findOne({shortId})
+   return res.json({totalClicks: result.visitHistory.length, analytics: result.visitHistory})
+}
+
+module.exports = {handleGenerateNewShortURL, handleGetAnalytics}
--- node/short-url/index.js
+++ node/short-url/index.js
@@ -1,13 +1,37 @@
-const express = require("express")
-const urlRoute = require('./routes/url')
-const {connectToMongoDB} = require('./connect')
-require('dotenv').config();
+const express = require("express");
+const urlRoute = require("./routes/url");
+const { connectToMongoDB } = require("./connect");
+const URL = require("./models/url");
+require("dotenv").config();
 
 const app = express();
-const PORT = 8001
+const PORT = 8001;
 
-connectToMongoDB(process.env.MONGODB_URL).then(
-    () => console.log("DB connected")
-)
+connectToMongoDB(process.env.MONGODB_URL).then(() =>
+  console.log("DB connected")
+);
 
-app.listen(PORT, () => console.log("Server started"))
+// middleware to parse incoming body
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log("Server started"));
