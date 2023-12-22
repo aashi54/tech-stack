c4adec517d39764dab3e9e361758c1e180d25bf8
aashi54 <asmitapatidar2023@gmail.com>
2023-12-22 21:07:15 +0530

    unit test for generating new URL and render home page

--- node/short-url/__tests__/controllers/url.test.js
+++ node/short-url/__tests__/controllers/url.test.js
@@ -1,16 +1,62 @@
 
-const {handleGenerateNewShortURL} = require('../../controllers/url');
+// const {handleGenerateNewShortURL} = require('../../controllers/url');
 
 
-jest.mock('../../models/url')
+// jest.mock('../../models/url')
 
-const request = {
-    body : {
-        url : "fakeurl.com"
-    }
-}
+// const request = {
+//     body : {
+//         url : "fakeurl.com"
+//     }
+// }
 
 
-test('Generate new short url', async () => {
-   await handleGenerateNewShortURL(request)
-})
+// test('Generate new short url', async () => {
+//    await handleGenerateNewShortURL(request)
+// })
+
+
+const { handleGenerateNewShortURL } = require('../../controllers/url');
+const URL = require('../../models/url');
+const shortid = require('shortid');
+
+jest.mock('shortid');
+jest.mock('../../models/url');
+
+describe('handleGenerateNewShortURL', () => {
+  test('should generate a new short URL and render home page', async () => {
+    const req = {
+      body: {
+        url: 'fakeurl.com',
+      },
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      render: jest.fn(),
+    };
+
+    // Mock shortid
+    const mockShortID = 'mockedShortID';
+    shortid.mockReturnValue(mockShortID);
+
+    await handleGenerateNewShortURL(req, res);
+
+    // Assertions
+    // expect(res.status).not.toHaveBeenCalled();
+    // expect(res.json).not.toHaveBeenCalled();
+
+    // Ensure URL.create is called with the expected parameters
+    expect(URL.create).toHaveBeenCalledWith({
+      shortId: mockShortID,
+      redirectURL: req.body.url,
+      visitedHistory: [],
+    });
+
+    // Ensure res.render is called with the expected parameters
+    expect(res.render).toHaveBeenCalledWith('home', { id: mockShortID });
+  });
+
+
+});
