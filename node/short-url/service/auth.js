4a2d63820352e8b2188674e7cfe21a0e133527a6
aashi54 <asmitapatidar2023@gmail.com>
2023-12-23 16:43:13 +0530

    stateless authentication

--- node/short-url/service/auth.js
+++ node/short-url/service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken')
+
+const secret = "Asmita@123$"
 
 function setUser(id,user){
-    return sessionIdToUserMap.set(id,user);
+   return jwt.sign({
+    _id : user._id,
+    email : user.email
+   }, secret)
+  
+
+
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    if(!token) return null;
+    try{
+        return jwt.verify(token, secret)
+    }catch(error){
+      return null;
+    }
+   
 }
 
 module.exports = {
