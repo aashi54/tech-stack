874855a15a48d71a25da23659071ccecef498a7b
aashi54 <asmitapatidar2023@gmail.com>
2023-12-25 20:00:10 +0530

    created simple put api

--- node/simpleAPI/controllers/app.js
+++ node/simpleAPI/controllers/app.js
@@ -17,4 +17,29 @@ async function getAllUsers(req, res) {
     res.json(users);
 }
 
-module.exports = { registerNewUser, getAllUsers };
+async function editUser(req, res){
+  const {userId} = req.params;
+  const {email, username} = req.body;
+
+  if(!email && !username){
+    return res.status(400).json({error: "Atleast one field required"})
+  }
+
+  const userToUpadte = users.find((user) => user.id === userId);
+
+  if(!userToUpadte){
+    return res.status(400).json({error : "user not found"})
+  }
+
+  if(email){
+    userToUpadte.email = email
+  }
+
+  if(username){
+    userToUpadte.username = username
+  }
+
+  return res.json(userToUpadte)
+}
+
+module.exports = { registerNewUser, getAllUsers , editUser};
--- node/simpleAPI/index.js
+++ node/simpleAPI/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {registerNewUser, getAllUsers} = require("./controllers/app")
+const {registerNewUser, getAllUsers, editUser} = require("./controllers/app")
 
 const app = express();
 const PORT = 3000;
@@ -12,7 +12,8 @@ app.post('/api/register', registerNewUser)
 
 app.get('/api/users',getAllUsers )
 
-module.exports = app;
+app.put('/api/users/:id', editUser)
+
 
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
